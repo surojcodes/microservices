ff104ed0949171621156ab5ed98962b3e80b037a
Suroj M <surojcodes@gmail.com>
2026-02-16 19:11:39 -0700

    add docker compose file

--- bank/src/config/index.ts
+++ bank/src/config/index.ts
@@ -1,4 +1,4 @@
 export const URLS = {
-  ACCOUNTS_API_URL: "http://localhost:3001/accounts",
-  CUSTOMERS_API_URL: "http://localhost:3000/customers",
+  ACCOUNTS_API_URL: "http://account:3001/accounts",
+  CUSTOMERS_API_URL: "http://customer:3000/customers",
 } as const;
