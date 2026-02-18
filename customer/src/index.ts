201308294db5c26fde2bf6bee0df6d5b5d2abc7f
Suroj M <surojcodes@gmail.com>
2026-02-17 19:09:50 -0700

    add POST customers for adding new customer

--- customer/src/index.ts
+++ customer/src/index.ts
@@ -7,7 +7,12 @@ const app = express();
 
 configDotenv();
 const PORT = process.env.PORT;
-
+app.use(express.json());
+interface CreateCustomerDto {
+  customer_id: string;
+  name: string;
+  email: string;
+}
 interface CustomerDto {
   id: string;
   name: string;
@@ -51,6 +56,33 @@ app.get(
           email: customer.email,
         },
       });
+  },
+);
+app.post(
+  "/customers",
+  (req: Request<never, CustomerAPIRes, CreateCustomerDto>, res) => {
+    const reqBody = req.body;
+    if (!reqBody.name || !reqBody.email) {
+      res.status(400).json({
+        success: false,
+        message: "Non empty name and email are required ",
+      });
+    }
+    const customers = data.customers;
+    const newCustomer: CreateCustomerDto = {
+      customer_id: String(customers.length + 1),
+      email: reqBody.email,
+      name: reqBody.name,
+    };
+    customers.push(newCustomer);
+    res.status(201).json({
+      success: true,
+      data: {
+        id: newCustomer.customer_id,
+        email: newCustomer.email,
+        name: newCustomer.name,
+      },
+    });
   },
 );
 app.listen(PORT, () => {
