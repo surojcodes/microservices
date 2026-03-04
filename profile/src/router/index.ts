7159e8e4ef61098b8df3c1b997b5984cf7376b21
Suroj M <surojcodes@gmail.com>
2026-03-04 16:28:18 -0700

    add auth to routes

--- profile/src/router/index.ts
+++ profile/src/router/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getProfile, getProfiles } from "../controllers";
+import { authenticateRequest } from "../middleware/auth";
 
 const profileRouter = express.Router();
 
-profileRouter.route("/").get(getProfiles);
-profileRouter.route("/:id").get(getProfile);
+profileRouter.route("/").get(authenticateRequest, getProfiles);
+profileRouter.route("/:id").get(authenticateRequest, getProfile);
 
 export default profileRouter;
