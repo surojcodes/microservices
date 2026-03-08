13be1f44839bda22cd0e8a289e6716974f975c24
Suroj M <surojcodes@gmail.com>
2026-03-07 18:30:47 -0700

    add health routes

--- accounts/src/index.ts
+++ accounts/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import accountRouter from "./routes";
+import healthRouter from "./routes/health";
 
 configDotenv();
 const PORT = process.env.PORT;
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/accounts", accountRouter);
+app.use("/", healthRouter);
 
 app.listen(PORT, () => {
   console.log(`ACCOUNT_API is listening on PORT ${PORT}`);
