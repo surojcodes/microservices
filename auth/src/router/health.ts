e5038610add337df8ca9eda716c4f445038600c2
Suroj M <surojcodes@gmail.com>
2026-03-07 18:40:14 -0700

    log health check endpoint call

--- auth/src/router/health.ts
+++ auth/src/router/health.ts
@@ -3,10 +3,12 @@ import express from "express";
 const healthRouter = express.Router();
 
 healthRouter.get("/health", (req, res) => {
+  console.log("Health check endpoint called");
   res.status(200).send("OK");
 });
 
 healthRouter.get("/ready", (req, res) => {
+  console.log("Ready check endpoint called");
   res.status(200).send("READY");
 });
 
