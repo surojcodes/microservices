3aa618ea44e8b7d59df28fb78cf9cd3d604b6ade
Suroj M <surojcodes@gmail.com>
2026-03-07 18:35:02 -0700

    add health routes

--- profile/src/index.ts
+++ profile/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import profileRouter from "./router";
+import healthRouter from "./router/health";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use("/profiles", profileRouter);
+app.use("/", healthRouter);
 
 app.listen(PORT, () => {
   console.log(`PROFILE_API Listening on ${PORT}`);
