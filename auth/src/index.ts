2b6391769831b7e4a22aaab6d30ebaf88b06fb0c
Suroj M <surojcodes@gmail.com>
2026-03-07 18:31:26 -0700

    add health routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import authRouter from "./router";
 import cookieParser from "cookie-parser";
+import healthRouter from "./router/health";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
 app.use("/auth", authRouter);
+app.use("/", healthRouter);
 
 app.listen(PORT, () => {
   console.log(`AUTH_API listening on ${PORT}`);
