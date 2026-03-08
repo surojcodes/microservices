ffb88633dae29f8e1618f7c6ddd24ebd269ad4f4
Suroj M <surojcodes@gmail.com>
2026-03-08 10:34:04 -0700

    update base service name in logger

--- accounts/src/logger/index.ts
+++ accounts/src/logger/index.ts
@@ -2,7 +2,7 @@ import pino from "pino";
 
 const logger = pino({
   base: {
-    service: "profile-service",
+    service: "account-service",
     env: process.env.NODE_ENV || "development",
   },
   level: process.env.LOG_LEVEL || "info",
