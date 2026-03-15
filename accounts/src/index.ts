6e30d4863e7726e4ce3482bee7bcab86964eb416
Suroj M <surojcodes@gmail.com>
2026-03-15 11:29:16 -0700

    metrics

--- accounts/src/index.ts
+++ accounts/src/index.ts
@@ -4,6 +4,8 @@ import accountRouter from "./routes";
 import healthRouter from "./routes/health";
 import logger from "./logger";
 import PinoHttp from "pino-http";
+import { httpRequestDuration, httpRequestTotal, register } from "./metrics";
+import { metricsMiddleware } from "./middleware/metrics";
 
 configDotenv();
 const PORT = process.env.PORT;
@@ -11,6 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(PinoHttp({ logger }));
+app.use(metricsMiddleware);
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 app.use("/accounts", accountRouter);
 app.use("/", healthRouter);
 
