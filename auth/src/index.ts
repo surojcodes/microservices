22a5a4289805e84a6774ce70722ce9297605dc04
Suroj M <surojcodes@gmail.com>
2026-03-15 11:29:28 -0700

    add metrics

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import healthRouter from "./router/health";
 import logger from "./logger";
 import PinoHttp from "pino-http";
+import { metricsMiddleware } from "./middleware/metrics";
+import { register } from "./metrics";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,7 +14,12 @@ const PORT = process.env.PORT;
 app.use(cookieParser());
 app.use(PinoHttp({ logger }));
 app.use(express.json());
+app.use(metricsMiddleware);
 app.use("/auth", authRouter);
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 app.use("/", healthRouter);
 
 app.listen(PORT, () => {
